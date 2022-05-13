bb1afd5ac82c0e220c4902586db76b7cc7f5c675
Adnan Pedroso <adnan_silva54@hotmail.com>
2022-05-13 14:06:25 -0300

    pagina de login

---
+++ src/VeioACalhar/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VeioACalhar.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult SignIn(string userName, string password)
+    {
+        return View();
+    }
+}
