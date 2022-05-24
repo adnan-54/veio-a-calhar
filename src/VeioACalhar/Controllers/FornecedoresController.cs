b7aec1bf961cafca66d64055940aad976ba01079
Adnan Pedroso <adnan_silva54@hotmail.com>
2022-05-24 11:41:14 -0300

    crud fornecedores

---
+++ src/VeioACalhar/Controllers/FornecedoresController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VeioACalhar.Models;
+using VeioACalhar.Repositories;
+
+namespace VeioACalhar.Controllers;
+
+public class FornecedoresController : Controller
+{
+    private readonly IFornecedorRepository fornecedorRepository;
+
+    public FornecedoresController(IFornecedorRepository fornecedorRepository)
+    {
+        this.fornecedorRepository = fornecedorRepository;
+    }
+
+    public IActionResult Index(string? search)
+    {
+        var fornecedores = fornecedorRepository.GetAll();
+
+        if (!string.IsNullOrEmpty(search))
+            fornecedores = fornecedores.Where(x => x.Nome.Contains(search, StringComparison.CurrentCultureIgnoreCase)).ToList();
+
+        return View(fornecedores);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(IFormCollection collection)
+    {
+        try
+        {
+            var telefones = new List<Telefone>
+            {
+                new() { Numero = collection["Telefone"] }
+            };
+
+            var enderecos = new List<Endereco>
+            {
+                new() { Logradouro = collection["Endereco"] }
+            };
+
+            var fornecedor = new Fornecedor
+            {
+                Nome = collection["Nome"],
+                NomeFantasia = collection["NomeFantasia"],
+                InscricaoEstadual = collection["InscricaoEstadual"],
+                Cnpj = collection["Cnpj"],
+                Pix = collection["Pix"],
+                Email = collection["Email"],
+                Observacoes = collection["Observacoes"],
+                Telefones = telefones,
+                Enderecos = enderecos
+            };
+
+            fornecedorRepository.Create(fornecedor);
+
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+
+    public IActionResult Details(int id)
+    {
+        var fornecedor = fornecedorRepository.Get(id);
+        if (fornecedor.Id > 0)
+            return View(fornecedor);
+        return View("NotFound");
+    }
+
+    public IActionResult Edit(int id)
+    {
+        var fornecedor = fornecedorRepository.Get(id);
+        if (fornecedor.Id > 0)
+            return View(fornecedor);
+        return View("NotFound");
+    }
+
+    [HttpPost]
+    public IActionResult Edit(int id, IFormCollection collection)
+    {
+        var telefones = new List<Telefone>
+        {
+            new() { Numero = collection["Telefone"] }
+        };
+
+        var enderecos = new List<Endereco>
+        {
+            new() { Logradouro = collection["Endereco"] }
+        };
+
+        var fornecedor = new Fornecedor
+        {
+            Id = id,
+            Nome = collection["Nome"],
+            NomeFantasia = collection["NomeFantasia"],
+            InscricaoEstadual = collection["InscricaoEstadual"],
+            Cnpj = collection["Cnpj"],
+            Pix = collection["Pix"],
+            Email = collection["Email"],
+            Observacoes = collection["Observacoes"],
+            Telefones = telefones,
+            Enderecos = enderecos
+        };
+
+        fornecedorRepository.Update(fornecedor);
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Delete(int id)
+    {
+        var fornecedor = fornecedorRepository.Get(id);
+        if (fornecedor.Id > 0)
+            return View(fornecedor);
+        return View("NotFound");
+    }
+
+    [HttpPost]
+    public IActionResult Delete(int id, IFormCollection collection)
+    {
+        var fornecedor = new Fornecedor
+        {
+            Id = id
+        };
+
+        fornecedorRepository.Delete(fornecedor);
+
+        return RedirectToAction(nameof(Index));
+    }
+}
--- veio_a_calhar_db.sql
+++ veio_a_calhar_db.sql
@@ -4,63 +4,63 @@ CREATE DATABASE veio_a_calhar_db
 USE veio_a_calhar_db
 
 CREATE TABLE Pessoas(
-    Id INT PRIMARY KEY IDENTITY,
-    Nome VARCHAR(32) NOT NULL,
-    Observacoes VARCHAR(MAX),
+	Id INT PRIMARY KEY IDENTITY,
+	Nome VARCHAR(32) NOT NULL,
+	Observacoes VARCHAR(MAX),
 	Pix VARCHAR(64),
 	Email VARCHAR(64),
 )
 
 CREATE TABLE Pessoas_Enderecos(
-    Id INT PRIMARY KEY IDENTITY,
-    Id_Pessoa INT FOREIGN KEY REFERENCES Pessoas(Id),
-    Logradouro VARCHAR(32) NOT NULL,
-    Numero INT NOT NULL,
-    Bairro VARCHAR(32) NOT NULL,
-    Cidade VARCHAR(16) NOT NULL,
-    Estado VARCHAR(16) NOT NULL,
-    Cep VARCHAR(9),
-    Observacoes VARCHAR(1024)
+	Id INT PRIMARY KEY IDENTITY,
+	Id_Pessoa INT FOREIGN KEY REFERENCES Pessoas(Id),
+	Logradouro VARCHAR(32) NOT NULL,
+	Numero INT NOT NULL,
+	Bairro VARCHAR(32) NOT NULL,
+	Cidade VARCHAR(16) NOT NULL,
+	Estado VARCHAR(16) NOT NULL,
+	Cep VARCHAR(9),
+	Observacoes VARCHAR(1024)
 )
 
 CREATE TABlE Pessoas_Telefones(
-    Id INT PRIMARY KEY IDENTITY,
-    Id_Pessoa INT FOREIGN KEY REFERENCES Pessoas(Id),
-    Numero VARCHAR(14) NOT NULL,
-    Observacoes VARCHAR(1024)
+	Id INT PRIMARY KEY IDENTITY,
+	Id_Pessoa INT FOREIGN KEY REFERENCES Pessoas(Id),
+	Numero VARCHAR(14) NOT NULL,
+	Observacoes VARCHAR(1024)
 )
 
 CREATE TABLE Pessoas_Juridicas(
-    Id INT PRIMARY KEY REFERENCES Pessoas(Id),
-    Nome_Fantasia VARCHAR(64),
-    Inscricao_Estadual VARCHAR(14),
-    Cnpj VARCHAR(14)
+	Id INT PRIMARY KEY REFERENCES Pessoas(Id),
+	Nome_Fantasia VARCHAR(64),
+	Inscricao_Estadual VARCHAR(14),
+	Cnpj VARCHAR(14)
 )
 
 CREATE TABLE Pessoas_Fisicas(
-    Id INT PRIMARY KEY REFERENCES Pessoas(Id),
-    Cpf VARCHAR(11),
-    Rg VARCHAR(12)
+	Id INT PRIMARY KEY REFERENCES Pessoas(Id),
+	Cpf VARCHAR(11),
+	Rg VARCHAR(12)
 )
 
 CREATE TABLE Clientes_Juridicos(
-    Id INT PRIMARY KEY REFERENCES Pessoas_Juridicas(Id),
+	Id INT PRIMARY KEY REFERENCES Pessoas_Juridicas(Id),
 )
 
 CREATE TABLE Clientes_Fisicos(
-    Id INT PRIMARY KEY REFERENCES Pessoas_Fisicas(Id),
+	Id INT PRIMARY KEY REFERENCES Pessoas_Fisicas(Id),
 )
 
 CREATE TABLE Usuarios(
-    Id INT PRIMARY KEY IDENTITY,
-    [Login] VARCHAR(50) NOT NULL,
+	Id INT PRIMARY KEY IDENTITY,
+	[Login] VARCHAR(50) NOT NULL,
 	Senha VARCHAR(2048) NOT NULL,
 	Data_Cadastro DATETIME NOT NULL,
 	Ativo BIT NOT NULL
 )
 
 CREATE TABLE Cargos(
-    Id INT PRIMARY KEY IDENTITY,
+	Id INT PRIMARY KEY IDENTITY,
 	Nome VARCHAR(32) NOT NULL,
 )
 
@@ -68,20 +68,20 @@ INSERT INTO Cargos VALUES ('Dono')
 INSERT INTO Cargos VALUES ('Ajudante')
 
 CREATE TABLE Funcionarios(
-    Id INT PRIMARY KEY REFERENCES Pessoas_Fisicas(Id),
-    Id_Cargo INT FOREIGN KEY REFERENCES Cargos(Id),
+	Id INT PRIMARY KEY REFERENCES Pessoas_Fisicas(Id),
+	Id_Cargo INT FOREIGN KEY REFERENCES Cargos(Id),
 	Id_Usuario INT FOREIGN KEY REFERENCES Usuarios(Id),
 	Salario INT NOT NULL
 )
 
 CREATE TABLE Fornecedores(
-    Id INT PRIMARY KEY REFERENCES Pessoas_Juridicas(Id),
+	Id INT PRIMARY KEY REFERENCES Pessoas_Juridicas(Id),
 )
 
 CREATE TABLE Unidades(
-    Id INT PRIMARY KEY IDENTITY,
-    Nome VARCHAR(32) NOT NULL,
-    Sigla VARCHAR(8) NOT NULL,
+	Id INT PRIMARY KEY IDENTITY,
+	Nome VARCHAR(32) NOT NULL,
+	Sigla VARCHAR(8) NOT NULL,
 )
 
 INSERT INTO Unidades VALUES ('Unitário', 'Un')
@@ -90,18 +90,18 @@ INSERT INTO Unidades VALUES ('Metros', 'm')
 INSERT INTO Unidades VALUES ('Metros Quadrados', 'm²')
 
 CREATE TABLE Produtos(
-    Id INT PRIMARY KEY IDENTITY,
-    Id_Fornecedor INT FOREIGN KEY REFERENCES Fornecedores(Id),
-    Id_Unidade INT FOREIGN KEY REFERENCES Unidades(Id),
-    Nome VARCHAR(32) NOT NULL,
-    Descricao VARCHAR(MAX) NOT NULL,
-    Preco_Custo MONEY NOT NULL,
-    Preco_Venda MONEY NOT NULL,
-    Quantidade DECIMAL(8, 2) NOT NULL
+	Id INT PRIMARY KEY IDENTITY,
+	Id_Fornecedor INT FOREIGN KEY REFERENCES Fornecedores(Id),
+	Id_Unidade INT FOREIGN KEY REFERENCES Unidades(Id),
+	Nome VARCHAR(32) NOT NULL,
+	Descricao VARCHAR(MAX) NOT NULL,
+	Preco_Custo MONEY NOT NULL,
+	Preco_Venda MONEY NOT NULL,
+	Quantidade DECIMAL(8, 2) NOT NULL
 )
 
 CREATE TABLE Formas_Pagamento(
-    Id INT PRIMARY KEY IDENTITY,
+	Id INT PRIMARY KEY IDENTITY,
 	Nome VARCHAR(32) NOT NULL,
 	Maximo_Parcelas INT NOT NULL,
 )
@@ -113,14 +113,14 @@ INSERT INTO Formas_Pagamento VALUES('Cartão Crédito', 12)
 INSERT INTO Formas_Pagamento VALUES('Cartão Débito', 1)
 
 CREATE TABLE Pagamentos(
-    Id INT PRIMARY KEY IDENTITY,
+	Id INT PRIMARY KEY IDENTITY,
 	Id_Pagador INT FOREIGN KEY REFERENCES Pessoas(Id),
 	Id_Favorecido INT FOREIGN KEY REFERENCES Pessoas(Id),
 	Id_Forma_Pagamento INT FOREIGN KEY REFERENCES Formas_Pagamento(Id),
 )
 
 CREATE TABLE Parcelas(
-    Id INT PRIMARY KEY IDENTITY,
+	Id INT PRIMARY KEY IDENTITY,
 	Id_Pagamento INT FOREIGN KEY REFERENCES Pagamentos(Id),
 	Numero INT NOT NULL,
 	Valor MONEY NOT NULL,
@@ -131,7 +131,7 @@ CREATE TABLE Parcelas(
 )
 
 CREATE TABLE Status_Transacaos(
-    Id INT PRIMARY KEY IDENTITY,
+	Id INT PRIMARY KEY IDENTITY,
 	[Status] VARCHAR(32) NOT NULL,
 )
 
@@ -141,44 +141,44 @@ INSERT INTO Status_Transacaos VALUES ('Concluído')
 INSERT INTO Status_Transacaos VALUES ('Cancelado')
 
 CREATE TABLE Transacoes(
-    Id INT PRIMARY KEY IDENTITY,
+	Id INT PRIMARY KEY IDENTITY,
 	Id_Pagamento INT FOREIGN KEY REFERENCES Pagamentos(Id),
 	Id_Status INT FOREIGN KEY REFERENCES Status_Transacaos(Id),
-    Data_Criacao DATETIME NOT NULL,
+	Data_Criacao DATETIME NOT NULL,
 	Data_Fechamento DATETIME,
 	Observacoes VARCHAR(MAX)
 )
 
 CREATE TABLE Transacoes_Produtos(
-    Id INT PRIMARY KEY IDENTITY,
-    Id_Transacoes INT REFERENCES Transacoes(Id),
-    Id_Produto INT REFERENCES Produtos(Id),
-    Quantidade DECIMAL(8, 2) NOT NULL,
-    Valor_Unitario MONEY NOT NULL,
+	Id INT PRIMARY KEY IDENTITY,
+	Id_Transacoes INT REFERENCES Transacoes(Id),
+	Id_Produto INT REFERENCES Produtos(Id),
+	Quantidade DECIMAL(8, 2) NOT NULL,
+	Valor_Unitario MONEY NOT NULL,
 	Desconto_Unitario INT NOT NULL
 )
 
 CREATE TABLE Vendas(
-    Id INT PRIMARY KEY REFERENCES Transacoes(Id),
+	Id INT PRIMARY KEY REFERENCES Transacoes(Id),
 	Previsao_Inicio DATETIME NOT NULL,
 	Previsao_Entrega DATETIME NOT NULL
 )
 
 CREATE TABLE Venda_Clientes(
-    Id INT PRIMARY KEY IDENTITY,
+	Id INT PRIMARY KEY IDENTITY,
 	Id_Venda INT FOREIGN KEY REFERENCES Vendas(Id),
 	Id_Cliente INT FOREIGN KEY REFERENCES Clientes(Id)
 )
 
 CREATE TABLE Venda_Funcionarios(
-    Id INT PRIMARY KEY IDENTITY,
+	Id INT PRIMARY KEY IDENTITY,
 	Id_Venda INT FOREIGN KEY REFERENCES Vendas(Id),
 	Id_Funcionario INT FOREIGN KEY REFERENCES Funcionarios(Id)
 )
 
 CREATE TABLE Compras(
-    Id INT PRIMARY KEY REFERENCES Transacoes(Id),
-    Id_Fornecedor INT FOREIGN KEY REFERENCES Fornecedores(Id),
+	Id INT PRIMARY KEY REFERENCES Transacoes(Id),
+	Id_Fornecedor INT FOREIGN KEY REFERENCES Fornecedores(Id),
 	Data_Compra DATETIME NOT NULL,
 	Data_Entrega DATETIME NOT NULL,
 )
