1cb3357ba4537f46f729357e790a72d86238d1eb
Adnan Pedroso <adnan_silva54@hotmail.com>
2022-06-02 00:56:25 -0300

    crud de funcionario

--- src/VeioACalhar/Controllers/UsuarioController.cs
+++ src/VeioACalhar/Controllers/UsuarioController.cs
@@ -39,7 +39,8 @@ public class UsuarioController : Controller
         }
     }
 
-    public IActionResult SignOut()
+    [Route("Usuario/SignOut")]
+    public IActionResult UserSignOut()
     {
         userService.SignOut();
         return RedirectToAction(nameof(HomeController.Index), "Home");
