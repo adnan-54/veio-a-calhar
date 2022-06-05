29a1b34f140da48506ca1dbfa1d8b742f051b3b2
Adnan Pedroso <adnan_silva54@hotmail.com>
2022-06-05 19:29:10 -0300

    fazer validações funcionar

--- src/VeioACalhar/Controllers/HomeController.cs
+++ src/VeioACalhar/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using VeioACalhar.ViewModels;
 
 namespace VeioACalhar.Controllers;
 
@@ -7,5 +9,11 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
